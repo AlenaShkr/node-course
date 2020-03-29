0c7180d701bd01da0c4479904e9c03384b756722
AlenaShkr <elalshkr@gmail.com>
2020-03-29 18:04:19 +0300

    my-caeser-cli/bug: fix method encode

--- task-1/my_caeser.js
+++ task-1/my_caeser.js
@@ -20,29 +20,21 @@ program.parse(process.argv);
 const { action, shift, input, output } = program.opts();
 
 class EncodeTransform extends stream.Transform {
-  constructor(key) {
+  constructor(value) {
     super();
-    this.key = key;
+    this.value = value;
   }
   _transform(data, encoding, callback) {
-    console.log(data.toString());
     this.push(
       data
         .toString()
         .replace(/[A-Z]/g, c =>
-          String.fromCharCode(((c.charCodeAt() - 65 + this.key) % 26) + 65)
+          String.fromCharCode(((c.charCodeAt() - 65 + this.value) % 26) + 65)
         )
         .replace(/[a-z]/g, c =>
-          String.fromCharCode(((c.charCodeAt() - 97 + this.key) % 26) + 97)
+          String.fromCharCode(((c.charCodeAt() - 97 + this.value) % 26) + 97)
         )
     );
-    callback();
-  }
-}
-
-class DecodeTransform extends stream.Transform {
-  _transform(data, encoding, callback) {
-    this.push(data.toString().toLowerCase());
     callback();
   }
 }
@@ -54,7 +46,7 @@ let writeStream;
 if (action === 'encode') {
   transform = new EncodeTransform(shift);
 } else if (action === 'decode') {
-  transform = new DecodeTransform();
+  transform = new EncodeTransform((26 - shift) % 26);
 } else if (action !== 'encode' || action !== 'decode') {
   process.stderr.write('wrong action');
   // eslint-disable-next-line no-process-exit
