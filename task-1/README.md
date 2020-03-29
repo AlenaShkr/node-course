e09080db25694cd8b21e1f16447bbc09b6cdcab3
AlenaShkr <elalshkr@gmail.com>
2020-03-29 15:24:25 +0300

    my-caeser-cli/feat: add streams without transform

---
+++ task-1/README.md
@@ -0,0 +1,13 @@
+Running application
+
+In terminal should write
+$ node my_caesar -a xxx -s xx -i xxx -o xxxx, 
+
+where
+-s, --shift: a shift
+-i, --input: an input file
+-o, --output: an output file
+-a, --action: an action encode/decode
+For example:
+
+$ node my_caesar -a encode -s 7 -i "./input.txt" -o "./output.txt"
--- task-1/my_caeser.js
+++ task-1/my_caeser.js
@@ -1,42 +1,77 @@
 const fs = require('fs');
 const path = require('path');
-const process = require('axios');
+const process = require('process');
 const stream = require('stream');
+// eslint-disable-next-line node/no-unsupported-features/node-builtins
+const { pipeline } = require('stream');
 const { program } = require('commander');
+
+program.storeOptionsAsProperties(false);
 
 program
   .requiredOption('-a, --action <string>', 'encode/decode')
-  .option('-s, --shift <value>', 'shift', 1)
+  .requiredOption('-s, --shift <value>', 'shift')
   .option('-i, --input <type>', 'infile')
   .option('-o, --output <filename>', 'outfile');
 
 program.parse(process.argv);
 
+// eslint-disable-next-line no-unused-vars
 const { action, shift, input, output } = program.opts();
 
-class UpperCaseTransform extends stream.Transform {
+class EncodeTransform extends stream.Transform {
   _transform(data, encoding, callback) {
     this.push(data.toString().toUpperCase());
-    console.log(shift);
     callback();
   }
 }
 
-if (action === 'encode' || action === 'decode') {
-  console.log(program.opts());
-  if (input) {
-    console.log(program.input);
-    const pathToRead = path.join(__dirname, `${input}`);
-    const readStream = fs.createReadStream(`${pathToRead}`, {
-      encoding: 'utf8'
-    });
-    const transform = new UpperCaseTransform();
-    if (output) {
-      const pathToWrite = path.join(__dirname, `${output}.txt`);
-      const write = fs.createWriteStream(pathToWrite);
-      readStream.pipe(transform).pipe(write);
-    } else readStream.on('data', chunk => console.log(chunk));
+class DecodeTransform extends stream.Transform {
+  _transform(data, encoding, callback) {
+    this.push(data.toString().toLowerCase());
+    callback();
   }
+}
+
+let transform;
+let readStream;
+let writeStream;
+
+if (action === 'encode') {
+  transform = new EncodeTransform();
+} else if (action === 'decode') {
+  transform = new DecodeTransform();
+} else if (action !== 'encode' || action !== 'decode') {
+  process.stderr.write('wrong action');
+  // eslint-disable-next-line no-process-exit
+  process.exit(1);
+}
+if (input === undefined) {
+  readStream = process.stdin;
+} else if (input.match(/.txt/)) {
+  const pathToRead = path.join(__dirname, `${input}`);
+  readStream = fs.createReadStream(`${pathToRead}`, {
+    encoding: 'utf8'
+  });
+}
+if (output !== undefined) {
+  const pathToWrite = path.join(__dirname, `${output}.txt`);
+  writeStream = fs.createWriteStream(pathToWrite);
+
+  pipeline(readStream, transform, writeStream, err => {
+    if (err) {
+      process.stderr.write('input file do not exist');
+    } else {
+      console.log('Pipeline succeeded.');
+    }
+  });
 } else {
-  console.log('do not enter action');
+  writeStream = process.stdout;
+  pipeline(readStream, transform, writeStream, err => {
+    if (err) {
+      process.stderr.write('input file do not exist');
+    } else {
+      process.stdout.write('Pipeline succeeded.');
+    }
+  });
 }
