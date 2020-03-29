0b74a572f6c75e53d1b7f2f3458ac53cd2088aa6
AlenaShkr <elalshkr@gmail.com>
2020-03-29 17:21:45 +0300

    my-caeser-cli/feat: add method encode

--- task-1/my_caeser.js
+++ task-1/my_caeser.js
@@ -9,8 +9,8 @@ const { program } = require('commander');
 program.storeOptionsAsProperties(false);
 
 program
-  .requiredOption('-a, --action <string>', 'encode/decode')
-  .requiredOption('-s, --shift <value>', 'shift')
+  .requiredOption('-a, --action <string>', 'encode/decode', 'encode')
+  .requiredOption('-s, --shift <value>', 'shift', 7)
   .option('-i, --input <type>', 'infile')
   .option('-o, --output <filename>', 'outfile');
 
@@ -20,8 +20,22 @@ program.parse(process.argv);
 const { action, shift, input, output } = program.opts();
 
 class EncodeTransform extends stream.Transform {
+  constructor(key) {
+    super();
+    this.key = key;
+  }
   _transform(data, encoding, callback) {
-    this.push(data.toString().toUpperCase());
+    console.log(data.toString());
+    this.push(
+      data
+        .toString()
+        .replace(/[A-Z]/g, c =>
+          String.fromCharCode(((c.charCodeAt() - 65 + this.key) % 26) + 65)
+        )
+        .replace(/[a-z]/g, c =>
+          String.fromCharCode(((c.charCodeAt() - 97 + this.key) % 26) + 97)
+        )
+    );
     callback();
   }
 }
@@ -38,7 +52,7 @@ let readStream;
 let writeStream;
 
 if (action === 'encode') {
-  transform = new EncodeTransform();
+  transform = new EncodeTransform(shift);
 } else if (action === 'decode') {
   transform = new DecodeTransform();
 } else if (action !== 'encode' || action !== 'decode') {
@@ -50,28 +64,23 @@ if (input === undefined) {
   readStream = process.stdin;
 } else if (input.match(/.txt/)) {
   const pathToRead = path.join(__dirname, `${input}`);
-  readStream = fs.createReadStream(`${pathToRead}`, {
-    encoding: 'utf8'
+  readStream = fs.createReadStream(pathToRead, {
+    encoding: 'utf-8'
+  });
+  readStream.on('error', err => {
+    if (err) process.stderr.write('file do not exist\n');
   });
 }
 if (output !== undefined) {
   const pathToWrite = path.join(__dirname, `${output}.txt`);
   writeStream = fs.createWriteStream(pathToWrite);
-
-  pipeline(readStream, transform, writeStream, err => {
-    if (err) {
-      process.stderr.write('input file do not exist');
-    } else {
-      console.log('Pipeline succeeded.');
-    }
-  });
 } else {
   writeStream = process.stdout;
-  pipeline(readStream, transform, writeStream, err => {
-    if (err) {
-      process.stderr.write('input file do not exist');
-    } else {
-      process.stdout.write('Pipeline succeeded.');
-    }
-  });
 }
+pipeline(readStream, transform, writeStream, err => {
+  if (err) {
+    process.stderr.write('Something wrong');
+  } else {
+    console.log('Pipeline succeeded.');
+  }
+});
